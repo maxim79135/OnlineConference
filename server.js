e389fa451cb83422d161afa4f1c451e098152d86
Max <maxim79135@gmail.com>
2020-12-21 18:18:58 +0300

    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cp = require('crypto');
 const formidable = require('formidable');
 
 const host = 'onlineconference2.tk';
-const port = 8080;
+const port = 443;
 
 const soil = "mystring";
 const pg = require('pg');
